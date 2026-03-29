097eefacc455a6605fcd3a7b4c7af6dfb25b16d2
Sonia Benazouz <sosoweb001@gmail.com>
2026-03-30 00:00:41 +0100

    Navbar

--- app/frontend/screens/SetupProfileScreen.tsx
+++ app/frontend/screens/SetupProfileScreen.tsx
@@ -3,6 +3,7 @@ import { Asset } from "expo-asset";
 import * as FileSystem from "expo-file-system/legacy";
 import { LinearGradient } from "expo-linear-gradient";
 import { useEffect, useState } from "react";
+import Navbar from "../components/Navbar";
 import {
   ActivityIndicator,
   FlatList,
@@ -307,6 +308,8 @@ export default function SetUpProfileScreen() {
           />
         ))}
       </View>
+       {/* ✅ Navbar */}
+      <Navbar active="home" onChange={() => {}} />
     </LinearGradient>
   );
 }

ea529dcaf5e96498372cd48a9ac0864ba26b3f21
Tinhinane Belarbi <tinhinane.belarbi@se.univ-bejaia.dz>
2026-04-05 17:19:41 +0100

    ajout de l'effet au pages questions et jai placé le cadenat de forget-passwrd au mileu

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,8 @@
 
-import Question4Screen from "./frontend/screens/Question1";
+import Question1Screen from "./frontend/screens/Question1";
 
 export default function Page() {
-  return <Question4Screen />;
+  return <Question1Screen />;
 }
  //import SetUpProfileScreen from "./frontend/screens/SetupProfileScreen";
 //export default function Page() {

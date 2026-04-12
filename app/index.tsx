d08cffaa6bc3f592f08d1e51586b4812b6e7193b
Tinhinane Belarbi <tinhinane.belarbi@se.univ-bejaia.dz>
2026-04-13 00:06:03 +0100

    page parametre + modale toppp

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,7 @@
 import { Redirect } from 'expo-router';
 export default function Page() {
 
-  return <Redirect href="/frontend/screens/SettingsScreen" />;
+  return <Redirect href="/frontend/screens/Start" />;
 
 
 
